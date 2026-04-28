054cf22c36a6e7ac96dfe27fa7152fa58166143e
melvin sevilla <melvin.sevilla@aedpay.com>
2026-04-27 20:42:50 -0600

    show areas from db

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home').then(m => m.Home)
-  },
-  {
-    path: 'hogar',
-    loadComponent: () => import('./pages/areas/areas').then(m => m.Areas)
-  },
-  {
-    path: 'autorepuestos',
-    loadComponent: () => import('./pages/areas/areas').then(m => m.Areas)
-  },
-  {
-    path: 'ferreteria',
-    loadComponent: () => import('./pages/areas/areas').then(m => m.Areas)
+    loadComponent: () => import('./pages/home/home').then(m => m.Home),
+    children: [
+      {
+        path: 'hogar',
+        loadComponent: () => import('./pages/areas/areas').then(m => m.Areas)
+      },
+      {
+        path: 'autorepuestos',
+        loadComponent: () => import('./pages/areas/areas').then(m => m.Areas)
+      },
+      {
+        path: 'ferreteria',
+        loadComponent: () => import('./pages/areas/areas').then(m => m.Areas)
+      }
+    ]
   }
 ];
